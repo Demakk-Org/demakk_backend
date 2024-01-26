2c952b476a615e44176a5e15b4b2880115c1997a
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2024-01-26 15:31:15 +0300

    DB-3 api endpoints update

--- src/controllers/userControllers/addAndSetAddress.js
+++ src/controllers/userControllers/addAndSetAddress.js
@@ -1,13 +1,16 @@
-import QueryByType from "../../utils/queryByType.js";
 import Address from "../../models/addressSchema.js";
 import User from "../../models/userSchema.js";
 import language from "../../../language.js";
+import { config } from "dotenv";
+import { ObjectId } from "bson";
+import { decode } from "jsonwebtoken";
+
+const LANG = config(process.cwd, ".env").parsed.LANG;
 
 const addAddress = async (req, res) => {
   let {
-    lang,
     type,
-    account,
+    lang,
     country,
     city,
     subCity,
@@ -17,18 +20,23 @@ const addAddress = async (req, res) => {
     postalCode,
   } = req.body;
 
+  const token = req.headers.authorization.split(" ")[1];
+  const { uid } = decode(token, "your_secret_key");
+
   if (!lang || !(lang in language)) {
-    lang = "en";
+    lang = LANG;
   }
 
-  var queryAndType = QueryByType(account);
-
-  if (!account || !type) {
-    return res.status(404).json({ message: language.eng.error[404] });
+  if (!ObjectId.isValid(uid)) {
+    return res.status(400).json({ message: language[lang].response[407] });
   }
 
-  User.findOne(queryAndType.searchQuery)
-    .then((user) => {
+  if (!type || type !== ("shippingAddress" && "billingAddress")) {
+    return res.status(400).json({ message: language[lang].response[400] });
+  }
+
+  try {
+    User.findById(uid).then((user) => {
       try {
         Address.create({
           uid: user._id,
@@ -39,30 +47,26 @@ const addAddress = async (req, res) => {
           uniqueIdentifier,
           streetAddress,
           postalCode,
-          updatedAt: new Date(),
-        })
-          .then(async (data) => {
-            if (type == "shippingAddress") {
-              user.shippingAddress = data._id;
-            } else if (type == "billingAddress") {
-              user.billingAddress = data._id;
-            }
-            user.save();
-            return res.status(200).json(data);
-          })
-          .catch((err) => {
-            console.log(err);
-            return res.status(500).json({ message: language[lang].error[200] });
-          });
+        }).then(async (data) => {
+          if (type == "shippingAddress") {
+            user.shippingAddress = data._id;
+          } else if (type == "billingAddress") {
+            user.billingAddress = data._id;
+          }
+          user.save();
+          return res
+            .status(200)
+            .json({ message: language[lang].response[201], data });
+        });
       } catch (error) {
         console.log(error.message);
-        return res.status(500).json({ message: language[lang].error[500] });
+        return res.status(500).json({ message: language[lang].response[500] });
       }
-    })
-    .catch((error) => {
-      console.log(error.message);
-      return res.status(404).json({ message: language[lang].error[500] });
     });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(404).json({ message: language[lang].response[500] });
+  }
 };
 
 export default addAddress;
--- src/middlewares/UserAuthentication.js
+++ src/middlewares/UserAuthentication.js
@@ -1,25 +1,31 @@
-import Jwt from "jsonwebtoken";
+import { decode } from "jsonwebtoken";
 import language from "../../language.js";
 import dotenv from "dotenv";
+import { ObjectId } from "bson";
 
 const LANG = dotenv.config(process.cwd, ".env").parsed.LANG;
 
 const UserAuthentication = (req, res, next) => {
   const token = req.headers?.authorization?.split(" ")[1];
+  const bearer = req.headers?.authorization?.split(" ")[0];
 
-  if (!token) {
+  if (!token || bearer !== "Bearer") {
     console.error("Authentication failed: No token provided");
     return res.status(401).json({ message: language[LANG].response[401] });
   }
-  const tokenValues = Jwt.decode(token, "your_secret_key");
+
+  const tokenValues = decode(token, "your_secret_key");
 
   if (!tokenValues) {
     console.error("Authentication failed: Invalid token");
     return res.status(401).json({ message: language[LANG].response[401] });
   }
 
-  let { exp, lang } = tokenValues;
-  console.log(lang);
+  let { exp, lang, uid } = tokenValues;
+
+  if (!uid || !ObjectId.isValid(uid)) {
+    return res.status(401).json({ message: language[LANG].response[407] });
+  }
 
   if (!lang || !(lang in language)) {
     lang = LANG;
