e0077465efb667fe53b4fa7c96f93b530b5254d9
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2024-04-19 00:48:40 +0300

    DB-11 more cors update

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ console.log(PORT);
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 mongoose
   .connect(MONGODB_ULI)
