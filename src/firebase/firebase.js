9ea60ee097c546808e1823aeed7e543ad72929ce
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2024-05-26 11:15:01 +0300

    DB-18 wrapped JSON.parse in try catch block

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,13 @@ import admin from "firebase-admin";
 
 import { config } from "dotenv";
 
-let serviceAccount = config(process.cwd, ".env").parsed.PRIVATE_KEY;
+let serviceAccount;
+
+try {
+  serviceAccount = config(process.cwd, ".env").parsed.PRIVATE_KEY;
+} catch (error) {
+  console.error(error);
+}
 
 export const app = admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(serviceAccount)),
