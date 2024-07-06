e18d9d60dc1310fc0500dcea8a781316c19091f4
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2024-07-06 18:25:24 +0300

    DB-18 from params to query

--- src/controllers/searchControllers/searchProducts.js
+++ src/controllers/searchControllers/searchProducts.js
@@ -11,7 +11,7 @@ import { Product } from "../../models/productSchema.js";
 const { LANG, LIMIT, PAGE, SORT } = config(process.cwd, ".env").parsed;
 
 const searchProducts = async (req, res) => {
-  let { page, limit, lang, sort, text, price } = req.params;
+  let { page, limit, lang, sort, text, price } = req.query;
 
   if (!lang || !(lang in responsse)) {
     lang = LANG;
