3bcc2f077b39caa3d96365a2b3e6b09fa27a1c82
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2024-07-08 12:32:03 +0300

    DB-18 removed deal and stock type image controller

--- src/routes/dealRoute.js
+++ src/routes/dealRoute.js
@@ -8,19 +8,10 @@ import deleteDeal from "../controllers/dealControllers/deleteDeal.js";
 import updateDeal from "../controllers/dealControllers/updateDeal.js";
 import getDeals from "../controllers/dealControllers/getDeals.js";
 import getDeal from "../controllers/dealControllers/getDeal.js";
-import addDealImages from "../controllers/dealControllers/addDealImage.js";
 
 const dealRoute = Router();
 
 dealRoute.post("/", AdminAuthentication, addDeal);
-dealRoute.post(
-  "/images",
-  ExpressFormidable({
-    multiples: true,
-  }),
-  AdminAuthentication,
-  addDealImages
-);
 dealRoute.delete("/", AdminAuthentication, deleteDeal);
 dealRoute.put("/", AdminAuthentication, updateDeal);
 dealRoute.get("/", getDeals);
